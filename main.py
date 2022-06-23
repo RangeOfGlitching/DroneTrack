c07a93b7d502603f325ac3df80e842441606fe84
dodo <jeffpangwei48@gmail.com>
2022-06-23 15:07:31 +0800

    update the track

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ try:
     while True:
         ret, bg_removed = dc.get_frame()
         frame, info, face = tr.findFace(bg_removed)
-        print(f"face: {face} size{info[1]}")
+        print(f"face: {face} size： {info[1]}")
         fbSpeed, yaw_speed, tr.preError = tr.track(info, face)
-        # error = cv2.imshow("color frame", frame)
-        #
-        # if cv2.waitKey(1) == ord("q"):
-        #     break
+        cv2.imshow("color frame", frame)
+        
+        if cv2.waitKey(1) == ord("q"):
+            break
 finally:
     dc.release()
